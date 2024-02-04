1cf027ef6080a81064320cbd82afa328a6df19e0
safimiazi <mohibullamiazi@gmail.com>
2024-02-04 21:00:27 +0600

    navbar init done

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Shared/Navbar";
+import Footer from "../Shared/Footer";
 
 const Root = () => {
     return (
         <div>
+            <Navbar/>
             <Outlet/>
+            <Footer/>
         </div>
     );
 };
