8a57aedc1df9ece30aeab0443643fd6b2c145872
safimiazi <mohibullamiazi@gmail.com>
2024-02-04 21:05:21 +0600

    footer completed

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -1,9 +1,11 @@
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className="footer footer-center p-4  text-base-content">
+        <aside>
+          <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+        </aside>
+      </footer>
     );
 };
 
