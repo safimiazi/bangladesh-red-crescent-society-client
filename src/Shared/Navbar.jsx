2681d6a560a5c2aa1ef2a29395fbf6c5b2d5f952
safimiazi <mohibullamiazi@gmail.com>
2024-02-04 21:28:34 +0600

    navbar completed

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
@@ -8,6 +9,7 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <Link to={"/"}> <li><a>Home</a></li></Link>
                         <li><a>Dashboard</a></li>
                     </ul>
                 </div>
@@ -15,12 +17,14 @@ const Navbar = () => {
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
+                    <Link to={"/"}> <li><a>Home</a></li></Link>
+
                     <li><a>Dashboard</a></li>
 
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="py-2 px-4 bg-green-400 rounded hover:bg-green-500">Register</a>
+                <Link to={"/register"}>                <a className="py-2 px-4 bg-green-400 rounded hover:bg-green-500">Register</a></Link>
             </div>
         </div>
     );
