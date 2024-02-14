b9bfeb0d7cbde6f1b67b193a98e64163edfbf0be
ashikurbd71 <ashikurovi2003@gmail.com>
2024-02-14 15:14:58 +0600

    Stock data form added

--- src/Pages/Dashboard/Form/StockManagesment/Addnewstock.jsx
+++ src/Pages/Dashboard/Form/StockManagesment/Addnewstock.jsx
@@ -1,150 +1,175 @@
-import React from "react";
-import { LuSaveAll } from "react-icons/lu";
-import { MdCancel } from "react-icons/md";
-import { FaArrowLeft } from "react-icons/fa6";
-import { Link } from "react-router-dom";
 import { Controller, useForm } from "react-hook-form";
+import "../../../../CustomCSS/HumanAssets.css";
 import Select, { components } from "react-select";
+import { useEffect, useRef, useState } from "react";
+import { Helmet } from "react-helmet";
+import { MdArrowCircleLeft } from "react-icons/md";
+import { Link } from "react-router-dom";
+
 const Addnewstock = () => {
+  // const [isImageSelected, setIsImageSelected] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    control,
+    setValue,
+    formState: { errors },
+  } = useForm();
 
-    // const [isImageSelected, setIsImageSelected] = useState(false);
-    const {
-      register,
-      handleSubmit,
-      control,
-      setValue,
-      formState: { errors },
-    } = useForm();
-  
-    // form data
-    const onSubmit = (data) => console.log(data)
-
-
-    const DropdownIndicator = (props) => {
-      return (
-        <components.DropdownIndicator {...props}>
-          <svg
-            className="dropSVG"
-            width="14"
-            height="8"
-            viewBox="0 0 14 8"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M7 8L0.937823 0.5L13.0622 0.499999L7 8Z" fill="#D9D9D9" />
-          </svg>
-        </components.DropdownIndicator>
-      );
+  // form data
+  const onSubmit = (data) => {
+    const StockData = {
+      item: data.Item,
+      warehouse: data.Warehouse,
+      available: data.Available,
+      quantitystock: data.Quantitystock,
+      date: data.Date,
+      
     };
-    // React select customization styles
-    const customStyles = {
-      control: (provided, state) => ({
-        ...provided,
-        // height: '40px',
-        // width: '369px',
-        // border: '1px solid #E6E6E6',
-        border: "none",
-        boxShadow: state.isFocused ? null : null, // Remove the box shadow on focus
-        outline: state.isFocused ? "1px solid #2AA778" : null, // Change the color here
-        "&:hover": {
-          outline: "1px solid #2AA778", // Change the color on hover
-        },
-        "&:hover .dropSVG path": {
-          fill: "#2AA778",
-        },
-      }),
-      menu: (provided) => ({
-        ...provided,
-        padding: "5px",
-      }),
-  
-      option: (provided, state) => ({
-        ...provided,
-        backgroundColor: state.isSelected ? "#2AA778" : "inherit", //  background color for selected option
-        color: state.isSelected ? "white" : "#777777E5", // text color for selected option
-        fontSize: "13px",
-        fontWeight: state.isSelected ? "700" : "600",
-        borderRadius: "5px",
-        marginTop: "2px",
-        "&:hover": {
-          backgroundColor: state.isSelected ? "#2AA778" : "#2AA778",
-          color: "white", // background color on hover
-        },
-      }),
-  
-      placeholder: (provided) => ({
-        ...provided,
-        fontSize: "13px",
-        color: "rgba(119, 119, 119, 0.63)",
-      }),
-  
-      singleValue: (provided) => ({
-        ...provided,
-        color: "#777777A1", // Change the color of the single value
-        fontSize: "14px", // Change the font size of the single value
-      }),
-    };
-  
-    // React Select filed provided options there
-    const prefixOption = [
-      { value: "Prefix Demo 1", label: "Prefix Demo 1" },
-      { value: "Prefix Demo 2", label: "Prefix Demo 2" },
-      { value: "Prefix Demo 3", label: "Prefix Demo 3" },
-    ];
+    console.log(StockData);
+  };
+  console.log(errors);
+
+  // Select field arrow svg replacement style
+  const DropdownIndicator = (props) => {
+    return (
+      <components.DropdownIndicator {...props}>
+        <svg
+          className="dropSVG"
+          width="14"
+          height="8"
+          viewBox="0 0 14 8"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M7 8L0.937823 0.5L13.0622 0.499999L7 8Z" fill="#D9D9D9" />
+        </svg>
+      </components.DropdownIndicator>
+    );
+  };
+  // React select customization styles
+  const customStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      // height: '40px',
+      // width: '369px',
+      // border: '1px solid #E6E6E6',
+      border: "none",
+      boxShadow: state.isFocused ? null : null, // Remove the box shadow on focus
+      outline: state.isFocused ? "1px solid #2AA778" : null, // Change the color here
+      "&:hover": {
+        outline: "1px solid #2AA778", // Change the color on hover
+      },
+      "&:hover .dropSVG path": {
+        fill: "#2AA778",
+      },
+    }),
+    menu: (provided) => ({
+      ...provided,
+      padding: "5px",
+    }),
+
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isSelected ? "#2AA778" : "inherit", //  background color for selected option
+      color: state.isSelected ? "white" : "#777777E5", // text color for selected option
+      fontSize: "13px",
+      fontWeight: state.isSelected ? "700" : "600",
+      borderRadius: "5px",
+      marginTop: "2px",
+      "&:hover": {
+        backgroundColor: state.isSelected ? "#2AA778" : "#2AA778",
+        color: "white", // background color on hover
+      },
+    }),
+
+    placeholder: (provided) => ({
+      ...provided,
+      fontSize: "13px",
+      color: "rgba(119, 119, 119, 0.63)",
+    }),
+
+    singleValue: (provided) => ({
+      ...provided,
+      color: "#777777A1", // Change the color of the single value
+      fontSize: "14px", // Change the font size of the single value
+    }),
+  };
+
+  // React Select filed provided options there
+
+
+  const Item = [
+    { value: "School Bag", label: "School Bag" },
+    { value: "Blanket", label: "Blanket" },
+    { value: "Turkish Tent", label: "Turkish Tent" },
+    { value: "Large Sacks", label: "Large Sacks" },
+   
+  ];
+
+  const Warehouse = [
+    { value: "Rangpur BDRCS", label: "Rangpur BDRCS" },
+    { value: "Kurigram BDRCS", label: "Kurigram BDRCS" },
+
+  ];
+
+
+  // For getting the value of the file input
   
   return (
-    <div>
-      <div className="flex justify-between items-center mb-20 lg:flex-row ">
-        <div className="flex justify-center items-center gap-2">
-          <h1 className=" text-[18px] underline font-medium text-[#2AA778]">
+    <>
+      <div>
+        <Helmet>
+          <title>BDRCS | Add New Stock </title>
+        </Helmet>
+        <div className="flex justify-between">
+          <p className="text-[20px] text-[#5C5C5C] ml-[8px] pb-4">
             Add New Stock
-          </h1>
+          </p>
 
-          <Link to={"/dashboard/stockinitem"}>
-            <div className="flex text-[#3C8DBC] justify-center items-center gap-1">
-              <FaArrowLeft className="text-sm" />
-              <h1 className="text-sm ">back to list</h1>
-            </div>
-          </Link>
+{/* bcak button */}
+         
+         
+        <Link to={'/dashboard/stockinitem'}>
+        <button className="bg-[#3C8DBC] w-[130px] px-0 text-[15px] font-samibold text-white h-[36px] rounded-[3px]">
+               
+               <div className="flex justify-center items-center gap-1">
+                 <MdArrowCircleLeft/>
+                <h1> Back To List</h1>
+               </div>
+              </button>
+      
+        
+        </Link>
+
         </div>
+        <div>
+          <form
+            className="bg-white max-w-screen-2xl xl:mx-auto p-8 pb-24 mb-8 rounded-[5px] ml-5 mr-5 "
+            onSubmit={handleSubmit(onSubmit)}
+          >
+         
 
-        {/* button */}
-        <div className="flex items-center gap-1">
-          <button className="bg-[#3C8DBC] lg:px-0 px-3 text-[15px] font-samibold text-white lg:w-[120px] h-[36px] text-center  rounded-[3px]">
-            <div className="flex gap-1 items-center justify-center">
-              <LuSaveAll /> <h1>Save</h1>
-            </div>
-          </button>
+            {/* grid section starts here */}
+            <div className="grid grid-cols-1  gap-y-6 gap-x-5 mt-5">
 
-          <button className="bg-[#DD4B39] lg:px-0 px-3 text-[15px] font-samibold text-white lg:w-[120px] h-[36px] text-center  rounded-[3px]">
-            <div className="flex gap-1 items-center justify-center">
-              <MdCancel /> <h1>Cancel</h1>
-            </div>
-          </button>
-        </div>
-      </div>
-
-      {/* form */}
-
-        <div className="flex bg-[#FFFFFF] p-5  justify-center items-center gap-3 flex-cols-4">
-             <div className=" w-[500px]">
-
-             <div className="flex items-center gap-3">
-                <p className="text-[18px] font-bold text-[#777777] mb-1 ml-[2px]">
-                  Item*
+           
+              {/* 11. Item G */}
+              <div>
+                <p className="text-[15px] text-[#777777] mb-1 ml-[2px]">
+                  1. Item
                 </p>
                 <Controller
-                  name="Prefix"
+                  name="Item"
                   control={control}
                   defaultValue=""
                   render={({ field }) => (
-                    // eslint-disable-next-line react/jsx-no-undef
                     <Select
-                      className="custom-select w-[400px]  h-[40px] border border-[#E6E6E6] rounded-[3px]"
+                      className="custom-select  w-full h-[40px] border border-[#E6E6E6] rounded-[3px]"
                       components={{ DropdownIndicator }}
-                      options={prefixOption}
-                      placeholder="Select Prefix"
-                      {...register("prefix")}
+                      options={Item}
+                      {...register("Item", { required: true })}
+                      placeholder="Select Item"
                       styles={customStyles}
                       onChange={(selectedOption) => {
                         field.onChange(selectedOption.value); // Pass only the value to react-hook-form
@@ -152,16 +177,97 @@ const Addnewstock = () => {
                     />
                   )}
                 />
-             </div>
               </div>
-            <div>
+              {/* 2. Warehouse */}
+              <div>
+                <p className="text-[15px] text-[#777777] mb-1 ml-[2px]">
+                  2. Warehouse
+                </p>
+                <Controller
+                  name="Warehouse"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <Select
+                      className="custom-select  w-full h-[40px] border border-[#E6E6E6] rounded-[3px]"
+                      components={{ DropdownIndicator }}
+                      {...register("Warehouse", { required: true })}
+                      options={Warehouse}
+                      placeholder="Select Warehouse"
+                      styles={customStyles}
+                      onChange={(selectedOption) => {
+                        field.onChange(selectedOption.value); // Pass only the value to react-hook-form
+                      }}
+                    />
+                  )}
+                />
+              </div>
+              {/* 3.Available*/}
+              <div>
+                <p className="text-[15px] text-[#777777] mb-1 ml-[2px]">
+                  3. Available
+                </p>
+                <input
+                  className="w-full h-[40px] border border-[#E6E6E6] rounded-[3px]"
+                  type="text"
+                  {...register("Available")}
+                  placeholder="0"
+                />
+              </div>
+              {/* 14. Quantity to stock*/}
+              <div>
+                <p className="text-[15px] text-[#777777] mb-1 ml-[2px]">
+                  4. Quantity to stock
+                </p>
+                <input
+                  className="w-full h-[40px] border border-[#E6E6E6] rounded-[3px]"
+                  type="text"
+                  {...register("Quantitystock")}
+                  placeholder="0"
+                />
+              </div>
+              {/* 5.Date */}
+              <div>
+                <p className="text-[15px] text-[#777777] mb-1 ml-[2px]">
+                  5.  Date
+                </p>
+                <input
+                  className="w-full h-[40px] border border-[#E6E6E6] rounded-[3px]"
+                  type="date"
+                  {...register("eDate")}
+                />
+              </div>
 
-           
+            
+  
 
+            
             </div>
-
+         
+          
+              <div className="flex mt-5 justify-end">
+              <div className="flex gap-6 mt-8 xl:mt-3">
+                <button className="bg-[#2AA778] w-[132px] h-[36px] rounded-[3px]">
+                  <input
+                    className="px-0 text-[15px] font-bold text-white"
+                    value="Save"
+                    type="submit"
+                  />
+                </button>
+                <button className="bg-[#878FA7] w-[132px] h-[36px] rounded-[3px]">
+                  <input
+                    className="px-0 text-[15px] font-bold text-white"
+                    value="Cancel"
+                    type="reset"
+                  />
+                </button>
+              </div>
+              </div>
+          
+          </form>
         </div>
-    </div>
+      </div>
+    </>
   );
 };
 
